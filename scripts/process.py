4cb159e75c2d1fe5411694fd4796aa540caf1eca
meghansaha <meghanha01@gmail.com>
2025-09-27 16:23:40 -0400

    practice with polars

--- scripts/process.py
+++ scripts/process.py
@@ -1,15 +1,47 @@
 # The data was originally found on https://osf.io/wpeh6/
 
-# Data is in a zip file, need to extract
-from zipfile import ZipFile
+#%%  Module Load-in--------------------------------------------------------
+#==========================================================================#
+from zipfile import ZipFile 
+from requests import get
+from io import BytesIO
+import polars as pl
 
-# Set the file name
-data_path = "data/data.zip"
+#==========================================================================#
+#%% Data Fetching----------------------------------------------------------
+url_zip = "https://osf.io/download/j48qf/"
+response = get(url_zip)
+lst_files = (
+    "data/demographics.csv",
+    "data/mood_reported.csv",
+    "data/update_current_state.csv"
+    )
 
-# Unzip it - create the zipfile object
-test = ZipFile(data_path)
+# My first function in Python!!
+# Will extract the document name from the file path
+def grab_file_name(string):
+    cleaned_string = string.removeprefix("data/").removesuffix(".csv")
+    return cleaned_string
 
-# Then extract it into the data folder
-test.extractall()
+# Create a dictionary to store dataframes
+dataframes = {}
 
-# Pick up here with choosing data for the plot and exploring polars
+# Iterate through to 
+with ZipFile(BytesIO(response.content)) as file_zip:
+    for file in lst_files:
+        clean_name = grab_file_name(file)
+        with file_zip.open(file) as csv_file:
+            dataframes[clean_name] = pl.read_csv(csv_file)
+
+#==========================================================================#
+#%% Data Wrangling----------------------------------------------------------
+# Players to be included in the visual
+df_players = (
+    dataframes["demographics"]
+    .filter(pl.col("responses").is_not_null())
+    .select("pid", "age")
+)
+
+
+
+# Q1: Progression amount -vs- well being score by age
